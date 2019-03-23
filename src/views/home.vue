e4fa7043eaccd39de89e9d875e1667a37da8f8e7
kejian-xu <709752479@qq.com>
2019-03-23 15:18:29 +0800

    import bootstrap

---
+++ src/views/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <span class='btn'>{{msg}}</span>
+    </div>
+</template>
+<script>
+import './../assets/css/style.css'
+export default {
+    data(){
+        return {
+            msg:'OK'
+        }
+    }
+}
+</script>
